264188e0ac4884cffb909675683759b132b45ea5
Wataru NISHIMOTO <watura@g91.org>
2012-11-27 07:45:57 +0900

    first commit

--- scrollLabel/ViewController.h
+++ scrollLabel/ViewController.h
@@ -2,12 +2,14 @@
 //  ViewController.h
 //  scrollLabel
 //
-//  Created by Nishimoto Wataru on 2012/11/27.
+//  Created by Nishimoto Wataru on 2012/11/26.
 //  Copyright (c) 2012年 Nishimoto Wataru. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UILabel *label1;
+@property (strong, nonatomic) IBOutlet UILabel *label2;
 
 @end
--- scrollLabel/ViewController.m
+++ scrollLabel/ViewController.m
@@ -2,7 +2,7 @@
 //  ViewController.m
 //  scrollLabel
 //
-//  Created by Nishimoto Wataru on 2012/11/27.
+//  Created by Nishimoto Wataru on 2012/11/26.
 //  Copyright (c) 2012年 Nishimoto Wataru. All rights reserved.
 //
 
@@ -17,7 +17,64 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+}
+-(void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+    NSString* text= @"Merry Christmas, Mr. Lawrence";
+    CGSize size = [self generateCGSize:text];
+    _label1.frame = CGRectMake(0,
+                               0,
+                               size.width,
+                               size.height);
+    
+    [_label2 setFrame:CGRectMake(size.width,
+                                 0,
+                                 size.width,
+                                 size.height)];
+    
+    _label1.text = _label2.text = text;
+    
+    [NSTimer scheduledTimerWithTimeInterval:0.1f
+                                     target:self
+                                   selector:@selector(scrollLabel:)
+                                   userInfo:nil
+                                    repeats:YES
+     ];
+}
+
+-(void)scrollLabel:(id)sender{
+    CGSize size = [self generateCGSize:_label1.text];
+    if (_label1.frame.origin.x < -1* (size.width+10)) {
+        _label1.frame = CGRectMake(size.width,
+                                   0,
+                                   size.width,
+                                   size.height);
+    }
+    if (_label2.frame.origin.x < -1* (size.width+10)) {
+        _label2.frame = CGRectMake(size.width,
+                                   0,
+                                   size.width,
+                                   size.height);
+    }
+    
+    
+    _label1.frame = CGRectMake(_label1.frame.origin.x-3,
+                               0,
+                               size.width,
+                               size.height);
+    [_label2 setFrame:CGRectMake(_label2.frame.origin.x-3,
+                                 0,
+                                 size.width,
+                                 size.height)];
+}
+
+- (CGSize)generateCGSize:(NSString *)str
+{
+    CGSize maxSize = CGSizeMake(10000, 100);
+    NSLineBreakMode breakMode = NSLineBreakByCharWrapping;
+    UIFont *font = [UIFont fontWithName:@"Helvetica-Bold" size:17];
+    return [str sizeWithFont:font constrainedToSize:maxSize lineBreakMode:breakMode];
 }
 
 - (void)didReceiveMemoryWarning
